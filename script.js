10367d5c7791c180b43188cdb4a4f17d3e6c600c
Raju kumar <117720041+Senrajkumar1998@users.noreply.github.com>
2023-10-27 13:11:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	var fname = document.getElementById("fname").value;
+	var lname = document.getElementById("lname").value;
 	
-	window.alert("John Doe");
+	window.alert(`${fname+lname}`);
 
 }
